076eb1cef3330a7723451d502fd2e0efb3343d50
subuta <shunta.3110@gmail.com>
2021-11-02 15:57:56 +0900

    Fix build error.

--- next.config.js
+++ next.config.js
@@ -1,23 +1,22 @@
 const { default: getConfig } = require("next/config");
 
+const rewriteRoutes = [
+  {
+    source: "/articles/:article",
+    destination: "/article",
+  },
+  // Order matters for "wider match"
+  {
+    source: "/:prefectureName/:city",
+    destination: "/city",
+  },
+];
+
 module.exports = {
   serverRuntimeConfig: {
-    rewriteRoutes: [
-      {
-        source: "/articles/:article",
-        destination: "/article",
-      },
-      // Order matters for "wider match"
-      {
-        source: "/:prefectureName/:city",
-        destination: "/city",
-      },
-    ],
+    rewriteRoutes,
   },
   async rewrites() {
-    // Get shared "rewriteRoutes" config through "serverRuntimeConfig".
-    const { serverRuntimeConfig } = getConfig();
-    const { rewriteRoutes } = serverRuntimeConfig;
     return rewriteRoutes;
   },
 };
